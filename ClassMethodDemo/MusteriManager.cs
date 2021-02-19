ded082950a6161cb96453251be5971db64508838
BURCU PINARBASI <brcupnrbs@gmail.com>
2021-02-19 12:29:59 +0300

    first commit

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            musteri.Id = 1234;
+            musteri.Name = "Burcu";
+            musteri.Surname = "Pinarbasi";
+            Console.WriteLine("eklendi.");
+
+        }
+
+        public void listele(Musteri musteri)
+        {
+            Console.WriteLine("musteri ıd="+musteri.Id);
+            Console.WriteLine("musteri ad="+musteri.Name);
+            Console.WriteLine("musteri soyad="+musteri.Surname);
+        }
+
+        public void sil()
+        {
+            Console.WriteLine("musteri silindi");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            Musteri [] musteriler = new Musteri[] { };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.listele(musteri);
+            musteriManager.sil();
+
+
+            Console.WriteLine("tamamlandı.");
+
+
+        }
+    }
+}
